159e3486cc9bceb0f0b381f45c0685d42ca2785d
本村煌雅/Motokin <human.kagoshima.kouga@gmail.com>
2025-10-10 13:31:30 +0900

    Debug.Logの追加

--- Move-MineBomber Unity/Assets/02_Scripts/_Outgame/=Motomura=/GameEndJudgement.cs
+++ Move-MineBomber Unity/Assets/02_Scripts/_Outgame/=Motomura=/GameEndJudgement.cs
@@ -4,28 +4,39 @@ using UnityEngine.SceneManagement;
 public class GameEndJudgement : MonoBehaviour
 {
     private bool _IssStartChange = false;
+
+    void Start()
+    {
+        _IssStartChange = false;
+    }
+
     void Update()
     {
         switch (GameManager.Instance.currentGameState)
         {
             case GameState.GameClear:
                 ClearSceneLoad();
+                Debug.Log("<color=Green>クリア判定</color>");
                 break;
             case GameState.GameOver:
                 GameOverSceneLoad();
+                Debug.Log("<color=Red>ゲームオーバー判定</color>");
+                break;
+            default:
+                Debug.Log("<color=Yellow>どちらでもない</color> 現在→"+ GameManager.Instance.currentGameState);
                 break;
         }
     }
     void ClearSceneLoad()
     {
-        if (!_IssStartChange) return;
+        if (_IssStartChange) return;
         SceneManager.LoadSceneAsync("GameClear");
         _IssStartChange = true;
     }
 
     void GameOverSceneLoad()
     {
-        if (!_IssStartChange) return;
+        if (_IssStartChange) return;
         SceneManager.LoadSceneAsync("GameOver");
         _IssStartChange = true;
     }
